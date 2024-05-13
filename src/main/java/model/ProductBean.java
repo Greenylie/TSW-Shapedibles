9f145bdecc628ce135e7448f37ff06027a70cdb1
stefzito8 <s.zito9@studenti.unisa.it>
2024-05-13 22:32:18 +0200

    Correzione errore: Product Bean
    Per errore non ho incluso la versione aggiornata di ProductBeans nel
    ultimo Commit, questo serve a risolvere il problema

--- src/main/java/model/ProductBean.java
+++ src/main/java/model/ProductBean.java
@@ -1,5 +1,90 @@
 package model;
 
-public class ProductBin {
+import java.io.Serializable;
 
+public class ProductBean implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+	String codice;
+	String nome;
+	double costo;
+	String descrizione;
+	int disponibilità;
+	String tipologia;
+	
+	public ProductBean()
+	{
+	 codice= " ";
+     nome= " ";
+	 costo= 0.0;
+	 descrizione= " ";
+	 disponibilità= 1;
+	 tipologia= " ";
+	}
+	
+	String getCodice()
+	{
+		return codice;
+	}
+	
+	String getNome()
+	{
+		return nome;
+	}
+	
+	double getCosto()
+	{
+		return costo;
+	}
+	
+	String getDescrizione()
+	{
+		return descrizione;
+	}
+	
+	int getDisponibilità()
+	{
+		return disponibilità;
+	}
+	
+	String getTipologia()
+	{
+		return tipologia;
+	}
+	
+	void setCodice(String codice)
+	{
+		this.codice=codice;
+	}
+	
+	void setNome (String nome)
+	{
+		this.nome=nome;
+	}
+	
+	void setCosto (double costo)
+	{
+		this.costo=costo;
+	}
+	
+	void setDescrizione (String descrizione)
+	{
+		this.descrizione=descrizione;
+	}
+	
+	void setDisponibilità (int disponibilità)
+	{
+		this.disponibilità=disponibilità;
+	}
+	
+	void setTipologia (String tipologia)
+	{
+		this.tipologia=tipologia;
+	}
+	 
+	@Override
+	public String toString() {
+		return nome + " (" + codice + "), " + costo + " " + tipologia + ""+ disponibilità + ". " + descrizione;
+	}
+	
 }
